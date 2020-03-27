2f21013e1d04e9f945b5d783e2e46433e3e2402f
chad.mckee <chad.mckee@slalom.com>
2020-03-27 11:11:39 -0400

    refactor file service

--- src/inois/services/file_service.py
+++ src/inois/services/file_service.py
@@ -10,14 +10,14 @@ class FileService:
     def validate_files(cls, config):
         logging.info("Validating Files...")
         cls.navigate_to_working_directory(config)
-        cls.validate_files_are_csv(config)
+        cls.validate_csv_file_extensions(config)
         cls.validate_files_exist(config)
 
     @staticmethod
     def navigate_to_working_directory(config):
         logging.debug("navigating to working directory")
         if not os.path.exists(config.WORKING_DIRECTORY):
-            logging.error(Notifications.DIRECTORY_NOT_FOUND_ERROR.format(os.path.join(config.WORKING_DIRECTORY)))
+            logging.error(Notifications.DIRECTORY_NOT_FOUND_ERROR.format(config.WORKING_DIRECTORY))
             raise OSError(Notifications.DIRECTORY_NOT_FOUND_ERROR.format(config.WORKING_DIRECTORY))
         os.chdir(config.WORKING_DIRECTORY)
         config.WORKING_DIRECTORY = os.path.realpath(os.getcwd())
@@ -25,23 +25,31 @@ class FileService:
         print(Notifications.ACTIVE_WORKING_DIRECTORY.format(config.WORKING_DIRECTORY))
 
     @staticmethod
-    def validate_files_are_csv(config):
+    def validate_csv_file_extensions(config):
         logging.debug("validating csv file extensions")
-        if "*" in config.FILES:
-            config.FILES = glob.glob("*.csv")
-            if len(config.FILES) == 0:
-                logging.error(Notifications.NO_CSV_FILES_FOUND_IN_FOLDER_ERROR.format(config.WORKING_DIRECTORY))
-                raise FileNotFoundError(Notifications.NO_CSV_FILES_FOUND_IN_FOLDER_ERROR.format(config.WORKING_DIRECTORY))
-
-        else:
+        if "*" not in config.FILES:
             for file in config.FILES:
                 if not file[-4:] == ".csv":
                     logging.error((Notifications.NON_CSV_FILE_EXTENSION_ERROR.format(file)))
                     raise TypeError(Notifications.NON_CSV_FILE_EXTENSION_ERROR.format(file))
 
-    @staticmethod
-    def validate_files_exist(config):
+    @classmethod
+    def validate_files_exist(cls, config):
         logging.debug("validating files exist")
+        if "*" in config.FILES:
+            cls.find_all_csv_files_in_working_directory(config)
+        else:
+            cls.locate_csv_files_from_config(config)
+
+    @staticmethod
+    def find_all_csv_files_in_working_directory(config):
+        config.FILES = glob.glob("*.csv")
+        if len(config.FILES) == 0:
+            logging.error(Notifications.NO_CSV_FILES_FOUND_IN_FOLDER_ERROR.format(config.WORKING_DIRECTORY))
+            raise FileNotFoundError(Notifications.NO_CSV_FILES_FOUND_IN_FOLDER_ERROR.format(config.WORKING_DIRECTORY))
+
+    @staticmethod
+    def locate_csv_files_from_config(config):
         for file in config.FILES:
             if not os.path.exists(os.path.join(config.WORKING_DIRECTORY, file)):
                 logging.error(Notifications.FILE_NOT_FOUND_ERROR.format(os.path.join(config.WORKING_DIRECTORY, file)))
@@ -49,4 +57,4 @@ class FileService:
                     Notifications.FILE_NOT_FOUND_ERROR.format(os.path.join(config.WORKING_DIRECTORY, file)))
             else:
                 logging.info(Notifications.FILE_FOUND.format(os.path.join(config.WORKING_DIRECTORY, file)))
-                print((Notifications.FILE_FOUND.format(os.path.join(config.WORKING_DIRECTORY, file))))
+                print(Notifications.FILE_FOUND.format(os.path.join(config.WORKING_DIRECTORY, file)))
